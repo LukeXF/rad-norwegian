92df5df647008942cb8a79fad2406e8cdf8450bc
Luke Brown <me@luke.sx>
2020-10-13 22:37:27 +0100

    RAD-3 Grid system setup and gatsby ImageSharp imports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path")
 
 /**
  * Configure your Gatsby site with this file.
@@ -18,7 +18,16 @@ module.exports = {
     authorLink: "https://github.com/LukeXF"
   },
   plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/assets/images`,
+        name: "images"
+      }
+    },
     "gatsby-plugin-remove-trailing-slashes",
     {
       resolve: `gatsby-plugin-manifest`,
@@ -47,19 +56,19 @@ module.exports = {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
         alias: {
-          '@components': path.resolve(__dirname, 'src/components'),
-          '@pages': path.resolve(__dirname, 'src/pages'),
-          '@reducers': path.resolve(__dirname, 'src/reducers'),
-          '@helpers': path.resolve(__dirname, 'src/helpers'),
-          '@actions': path.resolve(__dirname, 'src/actions'),
-          '@media': path.resolve(__dirname, 'src/media'),
-          '@styles': path.resolve(__dirname, 'src/styles'),
-          '@fonts': path.resolve(__dirname, 'src/fonts'),
-          '@views': path.resolve(__dirname, 'src/views'),
-          '@store': path.resolve(__dirname, 'src/store.js'),
+          "@components": path.resolve(__dirname, "src/components"),
+          "@pages": path.resolve(__dirname, "src/pages"),
+          "@reducers": path.resolve(__dirname, "src/reducers"),
+          "@helpers": path.resolve(__dirname, "src/helpers"),
+          "@actions": path.resolve(__dirname, "src/actions"),
+          "@media": path.resolve(__dirname, "src/media"),
+          "@styles": path.resolve(__dirname, "src/styles"),
+          "@fonts": path.resolve(__dirname, "src/fonts"),
+          "@views": path.resolve(__dirname, "src/views"),
+          "@store": path.resolve(__dirname, "src/store.js")
         },
-        extensions: [],
-      },
-    },
+        extensions: []
+      }
+    }
   ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,45 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "./styles.module.scss"
+import Img from "gatsby-image"
+import { Grid, Row, Col } from "@element-softworks/daintree"
 
-export default function Home() {
-  return <Layout>Hello2</Layout>
+export default function Home({
+                               data: { logo }
+                             }) {
+  return <Layout>
+    <div className={styles.home}>
+      <div className={styles.homeBackground}>
+        <Img imgStyle={{ objectFit: "contain" }} fluid={logo?.childImageSharp?.fluid}/>
+      </div>
+      <div className={styles.homeContent}>
+        <Grid>
+          <Row>
+            <Col sm={5}>
+              <h1>The Sound of Spain</h1>
+              <p>
+                Whether you're turning up the tunes in Tenerife, bouncing to the beat in Barcelona or moving to the
+                music in Madrid, we've got you covered.
+              </p>
+              <p>
+                You'll also have the opportunity to win a 5 day trip to Spain, courtesy of Norwegian!
+              </p>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    </div>
+  </Layout>
 }
+
+export const query = graphql`
+    query {
+        logo: file(relativePath: {regex: "/background.jpg/"}) {
+            childImageSharp {
+                fluid(maxWidth: 1200) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`
