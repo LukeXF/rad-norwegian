79e3dda785f5a187ea8faba8f8a1e80a0c0ec03c
Luke Brown <me@luke.sx>
2020-10-14 03:59:46 +0100

    RAD-7 HTML.js noScript fallback + manifest PWA update

--- src/components/DestinationTile/index.js
+++ src/components/DestinationTile/index.js
@@ -5,12 +5,20 @@ const DestinationTile = ({ onMouseEnter, onMouseLeave, ...destination }) => {
 	const { name, departure, departureCode, price, duration } = destination
 
 	return (
-		<button className={styles.destination} onMouseEnter={() => onMouseEnter(destination)} onMouseLeave={onMouseLeave}>
-			<div>
+		<button
+			className={styles.destination}
+			onMouseEnter={() => onMouseEnter(destination)}
+			onMouseLeave={onMouseLeave}
+		>
+			<a
+				target="_blank"
+				rel="noreferrer noopener"
+				href={`https://www.norwegian.com/uk/booking/flight-tickets/farecalendar/?D_City=${departureCode}&A_City=${name}`}
+			>
 				<h2>{name}</h2>
 				<hr />
 				Direct from {departure} ({departureCode}) £{price} {duration}*
-			</div>
+			</a>
 		</button>
 	)
 }
